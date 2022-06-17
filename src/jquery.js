914e50e38028ed40b46f4179309dd38bded439b4
baIder <511763880@qq.com>
2022-06-17 22:33:28 +0800

    delete some lines

--- src/jquery.js
+++ src/jquery.js
@@ -69,50 +69,6 @@ jQuery.fn = jQuery.prototype = {
   end() {
     return this.oldApi;
   },
-  addClass(className) {
-    for (let i = 0; i < this.elements.length; i++) {
-      this.elements[i].classList.add(className);
-    }
-    return this;
-  },
-  find(selector) {
-    let array = [];
-    for (let i = 0; i < this.elements.length; i++) {
-      array = array.concat(
-        Array.from(this.elements[i].querySelectorAll(selector))
-      );
-    }
-    array.oldApi = this;
-    return jQuery(array);
-  },
-  each(fn) {
-    for (let i = 0; i < this.elements.length; i++) {
-      fn.call(null, this.elements[i], i);
-    }
-    return this;
-  },
-  parent() {
-    const array = [];
-    this.each((node) => {
-      if (array.indexOf(node.parentNode) === -1) {
-        array.push(node.parentNode);
-      }
-    });
-    return jQuery(array);
-  },
-  children() {
-    const array = [];
-    this.each((node) => {
-      array.push(...node.children);
-    });
-    return jQuery(array);
-  },
-  print() {
-    console.log(this.elements);
-  },
-  end() {
-    return this.oldApi;
-  },
   get(index) {
     return this.elements[index];
   },
